668a98765bd291af5e01fa1a989deaf89491d9f8
Artyom Zhukov <artyom.zhukov@progforce.com>
2020-09-28 15:48:23 +0300

    finish html/css

--- js/sliders.js
+++ js/sliders.js
@@ -4,5 +4,15 @@ $('#slider-popular').slick({
     slidesToScroll: 1,
     dots: false,
     prevArrow: false,
-    nextArrow: false
+    nextArrow: false,
+    responsive: [
+        {
+            breakpoint: 450,
+            settings: {
+                slidesToShow: 1,
+                nextArrow: '<div class="button arrow arrow-right"><img src="./images/left.svg" alt="Arrow"></div>',
+                prevArrow: '<div class="button arrow arrow-left"><img src="./images/left.svg" alt="Arrow"></div>'
+            }
+        }
+    ]
 })
