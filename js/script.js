ad403db6379a9d4acb80efde9e26ffc8a37cabe4
Artyom Zhukov <artyom.zhukov@progforce.com>
2020-11-10 22:32:36 +0300

    fix for js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@ var shareButton = document.getElementById('share-button')
 var sharePalet = document.getElementById('share-palet')
 
 shareButton.addEventListener('click', function(event){
-    sharePalet.style.display = 'block';
+    event.preventDefault()
+    if(sharePalet.classList.contains('show-palet')){
+        sharePalet.classList.remove('show-palet')
+    }
+    else{
+        sharePalet.classList.add('show-palet')
+    }
 })
 
 document.body.addEventListener('click', function(event){
-    if(!sharePalet.contains(event.target)){
-        sharePalet.style.display = 'none';
+    if(!sharePalet.contains(event.target) && !shareButton.contains(event.target)){
+        sharePalet.classList.remove('show-palet')
     }
 }, true); 
